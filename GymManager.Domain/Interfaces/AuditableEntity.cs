5bf85b9db4fc90ef731e407f92f78517f4332a7e
dotad <dotad@BLACK_HACK>
2025-07-16 17:36:55 -0600

    se agrega una clase especifica para poder auditar los registros

---
+++ GymManager.Domain/Interfaces/AuditableEntity.cs
@@ -0,0 +1,62 @@
+﻿namespace GymManager.Domain.Interfaces
+{
+    public abstract class AuditableEntity
+    {
+        protected AuditableEntity() : this(
+        DateTime.Now,
+        string.Empty,
+        DateTime.Now,
+        string.Empty,
+        true,
+        DateTime.Now,
+        new DateTime(2999, 12, 31, 23, 59, 59).AddMilliseconds(999999)
+        )
+        {
+        }
+
+        protected AuditableEntity(DateTime creado, string creadoPor, DateTime modificado, string modificadoPor, bool vigente, DateTime vigenteDesde, DateTime vigenteHasta)
+        {
+            Creado = creado;
+            CreadoPor = creadoPor;
+            Modificado = modificado;
+            ModificadoPor = modificadoPor;
+            Vigente = vigente;
+            VigenteDesde = vigenteDesde;
+            VigenteHasta = vigenteHasta;
+        }
+        /// <summary>
+        /// Fecha y hora de creación de la entidad
+        /// </summary>
+        public DateTime Creado { get; protected set; }
+
+        /// <summary>
+        /// ID de la cuenta de usuario que creó esta entidad
+        /// </summary>
+        public string CreadoPor { get; protected set; }
+
+        /// <summary>
+        /// Fecha y hora de última modificación de la entidad
+        /// </summary>
+        public DateTime Modificado { get; protected set; }
+
+        /// <summary>
+        /// ID de la cuenta de usuario que modificó por última vez esta entidad
+        /// </summary>
+        public string ModificadoPor { get; protected set; }
+
+        /// <summary>
+        /// Indica si esta entidad se encuentra vigente
+        /// </summary>
+        public bool Vigente { get; protected set; }
+
+        /// <summary>
+        /// Fecha y hora en que esta entidad empieza a ser operativa en el sistema
+        /// </summary>
+        public DateTime VigenteDesde { get; protected set; }
+
+        /// <summary>
+        /// Fecha y hora en que esta entidad dejó de ser operativa en el sistema
+        /// </summary>
+        public DateTime VigenteHasta { get; protected set; }
+    }
+}
---
+++ GymManager.Domain/Interfaces/IAuditableEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManager.Domain.Interfaces
+{
+    public interface IAuditableEntity
+    {
+        DateTime Creado { get; set; }
+        string CreadoPor { get; set; }
+        DateTime Modificado { get; set; }
+        string ModificadoPor { get; set; }
+        bool Vigente { get; set; }
+        DateTime VigenteDesde { get; set; }
+        DateTime VigenteHasta { get; set; }
+    }
+}
