b9e8e5cbc68c80972cd7bf04df1a4b68733c89eb
dotad <dotad@BLACK_HACK>
2025-07-16 14:33:59 -0600

    se agrena nuggets para la validacion fluida y tambien para posteriormente implementar CQRS

---
+++ GymManager.Application/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManager.Application.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            // Register application services here
+            // Example: services.AddScoped<IMyService, MyService>();
+            services.AddValidatorsFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddMediatR(config => {
+                //services.AddScoped<IDomainEventDispatcher, DomainEventDispatcher>();
+                config.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+            });
+            return services;
+        }
+    }
+}
