26077ebeceeb8ee381d92bc4ff71f41d4d1c5393
dotad <dotad@BLACK_HACK>
2025-07-16 20:52:23 -0600

    se agregan unas clases

---
+++ GymManager.Domain/Entities/UserRole.cs
@@ -0,0 +1,24 @@
+﻿using GymManager.Domain.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManager.Domain.Entities
+{
+    public class UserRole
+        : IdentityUserRole<Guid>, IAuditableEntity
+    {
+        public DateTime Creado { get; set; } = DateTime.Now;
+        public string CreadoPor { get; set; } = string.Empty;
+        public DateTime Modificado { get; set; } = DateTime.Now;
+        public string ModificadoPor { get; set; } = string.Empty;
+        public bool Vigente { get; set; } = true;
+        public DateTime VigenteDesde { get; set; } = DateTime.Now;
+        public DateTime VigenteHasta { get; set; } = new DateTime(2999, 12, 31, 23, 59, 59).AddMilliseconds(999999);
+        public User User { get; set; } = default!;
+        public Role Role { get; set; } = default!;
+    }
+}
